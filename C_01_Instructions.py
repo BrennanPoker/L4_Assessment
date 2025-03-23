78f909333494a0c690ccff520e395da33e5c1f61
BrennanPoker <casillaa4159@masseyhigh.school.nz>
2025-03-24 09:25:31 +1300

    Ingame instructions

---
+++ C_01_Instructions.py
@@ -0,0 +1,76 @@
+# Check that users have entered a valid
+# option based on a list
+def string_checker(question, valid_ans=('yes', 'no')):
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # Get user response and make sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+
+            # check if the user response is the same as
+            # the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
+
+        # print error if user does not enter something that is valid
+        print(error)
+        print()
+
+
+# functions go here
+
+def yes_no(question):
+    while True:
+
+        response = input(question).lower()
+
+        # check the user says yes / no
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("please enter yes / no")
+
+
+# Displays instructions
+def instructions():
+    print('''
+*** Instructions ***
+
+To begin, choose the number of rounds (or press <enter> for
+infinite mode).
+
+Then play against the computer.  You need to choose R (rock),
+P (paper) or S (scissors).
+
+The rules are as follows:
+o   Paper beats rock
+o   Rock beats scissors
+o   Scissors beats paper
+
+Press <xxx> to end game at anytime.
+
+Good Luck!
+    ''')
+
+
+# Main routine
+print()
+print("Area and Perimeter Problems")
+print()
+# ask the user if they want instructions and display
+# if requested
+want_instructions = string_checker("Do you want to see the instructions? ")
+
+# checks user enter yes (y) or no (n)
+if want_instructions == "yes":
+    instructions()
+
+print("Program continues")
